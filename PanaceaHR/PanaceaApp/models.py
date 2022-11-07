f4d60e0bdfaea9359f913ca0b6ced60bba9b2333
1Nelsonel <nelsonmokoro37@gmail.com>
2022-11-07 17:51:09 +0300

    success

--- PanaceaHR/PanaceaApp/models.py
+++ PanaceaHR/PanaceaApp/models.py
@@ -7,11 +7,13 @@ class Employee(models.Model):
     dateofbirth = models.DateField(blank=True, null=True)
     gender = models.CharField(max_length=20, blank=True, null=True)
     idnumber = models.CharField(max_length=20, blank=True, null=True)
+    telephone = models.CharField(max_length=20, blank=True, null=True)
     idphoto = models.ImageField(upload_to='media', blank=True, null=True)
     nssfphoto = models.ImageField(upload_to='media', blank=True, null=True)
     nhifphoto = models.ImageField(upload_to='media', blank=True, null=True)
     updated = models.DateTimeField(auto_now=True, null=True)
     created = models.DateTimeField(auto_now_add=True, null=True)
+    
 
     class Meta:
         ordering = ['-updated', '-created']
